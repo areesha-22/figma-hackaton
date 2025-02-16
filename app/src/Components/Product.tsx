7662e46e44f4175bd65fec2e72155256219e2e8f
areesha-22 <110978549+areesha-22@users.noreply.github.com>
2025-02-16 21:33:34 +0500

    update

--- app/src/Components/Product.tsx
+++ app/src/Components/Product.tsx
@@ -1,35 +1,40 @@
-import React from 'react'
-
-import InfoCard from "../Components/InfoCard"
-
-import Link from 'next/link'
+import React from "react";
+import InfoCard from "../Components/InfoCard";
+import Link from "next/link";
 
 const Product = () => {
+  return (
+    <div className="pt-10 px-4">
+      <div className="text-gray-400 font-semibold text-base p-9 text-center md:text-left">
+        Popular Car
+      </div>
 
-return (
+      {/* Responsive Grid Layout */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ">
+        <InfoCard heading="Koenigsegg" sub="Sport" price="$99.00/day" ImageUrl="/car1.svg" icons=""/>
+        <InfoCard heading="Nissan GT-R" sub="Sport" price="$80.00/day" ImageUrl="/car2.svg" icons="" />
+        <InfoCard heading="Rolls-Royce" sub="Sport" price="$96.00/day" ImageUrl="/car3.svg"  icons=""/>
+        <InfoCard heading="Nissan GT-R" sub="Sport" price="$80.00/day" ImageUrl="/Car.svg"icons="" />
+        <InfoCard heading="All New Rush" sub="Sport" price="$72.00/day" ImageUrl="/car4.svg"icons="" />
+        <InfoCard heading="CR-V" sub="Sport" price="$72.00/day" ImageUrl="/Car5.svg" icons=""/>
+        <InfoCard heading="All New Terios" sub="Sport" price="$80.00/day" ImageUrl="/Car6.svg" icons=""/>
+        <InfoCard heading="CR-V" sub="Sport" price="$74.00/day" ImageUrl="/Car7.svg"icons="" />
+        <InfoCard heading="MG ZX Exclusive" sub="Sport" price="$80.00/day" ImageUrl="/car8.svg" icons=""/>
+        <InfoCard heading="New MG ZS" sub="Sport" price="$76.00/day" ImageUrl="/car9.svg"icons="" />
+        <InfoCard heading="MG ZX Excite" sub="Sport" price="$80.00/day" ImageUrl="/car8.svg"icons="" />
+        <InfoCard heading="New MG ZS" sub="Sport" price="$74.00/day" ImageUrl="/car9.svg"icons="" />
+      </div>
 
-<div className='pt-10'>
+      {/* Show More Button */}
+      <div className="flex justify-center mt-10">
+        <Link href="/front">
+          <button className="bg-[#3563E9] text-white w-40 h-11 rounded font-semibold text-xs hover:bg-[#2748b0] transition">
+            Show More Cars
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-<div className='text-gray-400 font-semibold text-base p-9 relative bottom-48 '>Popular Car</div>
-
-<div className='md:flex'> <InfoCard heading="Koenigsegg" sub="sport" price="\$99.00/day" icons='' ImageUrl='/car1.svg'/>
- <InfoCard heading="NissanGT-R" sub="sport" price="\$80.00/day" icons="" ImageUrl='/car2.svg'/>
-  <InfoCard heading="Rolls-Royce" sub="sport" price="\$96.00/day"icons='' ImageUrl='/car3.svg'/></div> <div className='md:flex'> 
-  <InfoCard heading="NissanGT-R" sub="sport" price="\$80.00/day"icons='' ImageUrl='/Car.svg'/> 
-  <InfoCard heading="All NewRush" sub="sport" price="\$72.00/ day"icons='' ImageUrl='/car4.svg'/> 
-  <InfoCard heading="CR-V" sub="sport" price="\$72.00/day"icons='' ImageUrl='/Car5.svg'/></div> <div className='md:flex'>
-    <InfoCard heading="All NewTerios" sub="sport" price="\$80.00/day"icons='' ImageUrl='/Car6.svg'/> 
-  <InfoCard heading="CR-V" sub="sport" price="\$74.00/day"icons='' ImageUrl='/Car 7.svg'/>
-   <InfoCard heading="MG ZXExclusice" sub="sport" price="\$80.00/day"icons='' ImageUrl='/car8.svg'/></div> <div className='md:flex'>
-     <InfoCard heading="New MGZS" sub="sport" price="\$76.00/day"icons='' ImageUrl='/car9.svg'/>
-      <InfoCard heading="MG ZX Excite" sub="sport" price="\$80.00/day"icons='' ImageUrl='/car8.svg'/>
-       <InfoCard heading="New MG ZS" sub="sport" price="\$74.00/day"icons='' ImageUrl='/car9.svg'/></div>
-  <div className="text-center justify-center mt-11 relative bottom-32">
-   <Link href='/front' > <button className="bg-[#3563E9] text-white w-40 h-11 rounded font-semibold text-xs">Show More Car</button></Link>
-  </div>
-</div>
-)
-
-}
-
-export default Product
+export default Product;
